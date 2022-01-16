c2eee313e1c6e9150bf2bbadc340b435f54fcaee
Stu Campbell <stucampbell.git@gmail.com>
2022-01-16 23:04:25 +0000

    Working on x86_64 and aarch64 linux.  Needs verification on Mac

--- build-function.sh
+++ build-function.sh
@@ -2,36 +2,31 @@
 
 kernel="$(uname)"
 arch="$(uname -m)"
-linker="x86_64-linux-musl-gcc"
+linker="x86_64-linux-gnu-gcc"
 rustflags=""
 
 case $kernel in
     Linux)
-        echo "OS = Linux\nArch = ${arch}"
-        linker="musl-gcc"
-        echo "Linker = ${linker}"
-        sudo apt install build-essential musl-tools musl-dev -y
-        rustflags="\nrustflags = [ \"-C\", \"target-feature=+crt-static\", \"-C\", \"link-arg=-lgcc\" ]"
-        # if [ "${arch}" = "aarch64" ]; then
-        #     sudo apt install gcc-x86-64-linux-gnu -y
-        # fi
+        echo "OS = Linux\nArch = ${arch}\nLinker = ${linker}"
+        sudo apt install build-essential -y
+        if [ "${arch}" = "aarch64" ]; then
+            export CC_x86_64_unknown_linux_musl=x86_64-linux-gnu-gcc
+            sudo apt install gcc-x86-64-linux-gnu -y
+        fi
         ;;
 
     Darwin)
         if [ "${arch}" = "x86_64" ]; then
             if [ "$(sysctl -in sysctl.proc_translated)" = "1" ]; then
-                echo "OS = Mac M1 using Rosetta\nArch = ${arch}"
-                echo "Linker = ${linker}"
+                echo "OS = Mac M1 using Rosetta\nArch = ${arch}\nLinker = ${linker}"
                 sudo apt install gcc-x86-64-linux-gnu -y
             else
-                echo "OS = Mac Intel\nArch = ${arch}"
-                echo "Linker = ${linker}"
+                linker="x86_64-linux-musl-gcc"
+                echo "OS = Mac Intel\nArch = ${arch}\nLinker = ${linker}"
                 ln -s /usr/local/bin/${linker} /usr/local/bin/musl-gcc
             fi
         elif [ "${arch}" = "arm64" ]; then
-            echo "OS = Mac M1\nArch = ${arch}"
-            linker="x86_64-linux-gnu-gcc"
-            echo "Linker = ${linker}"
+            echo "OS = Mac M1\nArch = ${arch}\nLinker = ${linker}"
             sudo apt install gcc-x86-64-linux-gnu -y
         fi        
         ;;

abbb8bed1a9aec6ef20db6ed71c6d289c2cc9c0f
Rhett Harrison <reharri7@asu.edu>
2022-08-03 17:07:26 -0700

    format passing

--- resources/src/commands.rs
+++ resources/src/commands.rs
@@ -2,5 +2,6 @@
 // you will need to declare them as public modules below.
 // This will allow you to import them into main.rs.
 // Read more here: https://doc.rust-lang.org/rust-by-example/mod.html
-pub mod ping;
+
 pub mod buns;
+pub mod ping;

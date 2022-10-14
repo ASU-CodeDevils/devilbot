1b98a4d1809c83ff4c6a9f4ed1096289015d3008
Rhett Harrison <reharri7@asu.edu>
2022-10-13 22:27:34 -0700

    Adding emoji_reaction.rs. Better variable name in conversations_open.rs

--- resources/src/commands/emoji_reaction.rs
+++ resources/src/commands/emoji_reaction.rs
@@ -1,22 +1,22 @@
 use reqwest::StatusCode;
 use serde_json::Value;
-// Opens a conversation. See https://api.slack.com/methods/conversations.open for request json examples
-pub async fn open_conversation(json: &Value) -> Value {
+// Sends an emoji reaction to the passed in json
+pub async fn reactions_add(json: &Value) -> Value {
     let devil_bot_auth_token: String = crate::get_env_var("SLACK_API_BOT_TOKEN");
     let client = reqwest::Client::new();
-    let channel_response = client
-        .post("https://slack.com/api/conversations.open")
-        .bearer_auth(&devil_bot_auth_token)
+    let emoji_response = client
+        .post("https://slack.com/api/reactions.add")
+        .bearer_auth(devil_bot_auth_token)
         .json(json)
         .send()
         .await
         .unwrap();
-    match channel_response.status() {
+    match emoji_response.status() {
         StatusCode::OK => println!("success!"),
         StatusCode::PAYLOAD_TOO_LARGE => {
             println!("Request payload is too large!");
         }
         s => println!("Received response status: {:?}", s),
     };
-    channel_response.json().await.unwrap()
+    emoji_response.json().await.unwrap()
 }

2bbb8600124cbb4690d9377ad1e035346a3f6336
Rhett Harrison <reharri7@asu.edu>
2022-11-05 11:56:47 -0700

    Added reaction event handler

--- resources/src/event_handlers/reaction_added_event_handler.rs
+++ resources/src/event_handlers/reaction_added_event_handler.rs
@@ -1,7 +1,66 @@
 use serde_json::Value;
 
-pub async fn handle_reaction_added_event(_body: &Value) {
-    // deconstruct the information needed from the event
+use crate::slack::add_reaction;
 
-    // match based on the values and call the appropriate command
+pub async fn handle_reaction_added_event(body: &Value) {
+    // deconstruct the information needed from the event
+    let reaction = body["event"]["reaction"]
+        .as_str()
+        .unwrap_or("invalid_reaction");
+    let channel = body["event"]["item"]["channel"]
+        .as_str()
+        .unwrap_or("invalid_channel");
+    let timestamp = body["event"]["item"]["ts"]
+        .as_str()
+        .unwrap_or("invalid_timestamp");
+
+    log::info!(
+        "Information parse from handle_reaction_added_event reaction: \
+    {}, channel {}, timestamp {}",
+        reaction,
+        channel,
+        timestamp
+    );
+    // Add any other emojis to copy against here
+    // Please keep alphabetized
+    let emojis_to_copy = vec![
+        "beer",
+        "buns",
+        "catjam",
+        "codedevils_flash",
+        "codedevils_rainbow_fast",
+        "distressed_salamander",
+        "fax",
+        "ferris_happy",
+        "fishjam",
+        "heart",
+        "kek",
+        "kekhands",
+        "kekleave",
+        "keks",
+        "kekthar",
+        "kekw",
+        "kekwoo",
+        "kekzos",
+        "party-ferris",
+        "party-pogfish-pogging",
+        "party-wizard",
+        "party_blob",
+        "pepehands",
+        "pepehands-6102",
+        "pepelol",
+        "peperun",
+        "pesfuckyou",
+        "pog",
+        "pogfish-fast",
+        "pogfish-pogging",
+        "rust_logo",
+        "sadkek",
+        "temple-of-rust",
+    ];
+    if emojis_to_copy.contains(&reaction) {
+        add_reaction::run(channel, timestamp, reaction)
+            .await
+            .unwrap_or_else(|err| log::info!("Error adding reaction: {}", err))
+    }
 }
